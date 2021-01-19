d8473c097e4b8d1b36e7f1de0311b3208ca3c32e
John Correia <johnjohnx4@gmail.com>
2021-01-18 20:17:32 -0500

    added Mission section

--- src/views/Mission/Mission.js
+++ src/views/Mission/Mission.js
@@ -1,8 +1,50 @@
-function App() {
+import HeroText from '../../components/TitleText';
+import Mission from './Mission.style';
+import missionbanner from '../../images/missionbanner.png';
+
+function MissionView() {
   return (
-    <div>
-    </div>
+    <Mission.Container>
+      <Mission.Title>
+        <HeroText
+          text={'SHARE YOUR IDEAS, WE’LL BRING THEM TO LIFE'}
+          size={80}
+          fullStop={true}
+          reversed={true}
+        />
+      </Mission.Title>
+
+      <Mission.Banner src={missionbanner} />
+
+      <Mission.Block.Container>
+        <Mission.Block.Column>
+          <Mission.Block.Title.Container>
+            <Mission.Block.Title.Bold>
+              We are a startup commited to
+            </Mission.Block.Title.Bold>
+            <Mission.Block.Title.Emphasis>
+              startups
+            </Mission.Block.Title.Emphasis>
+          </Mission.Block.Title.Container>
+        </Mission.Block.Column>
+
+        <Mission.Block.Column>
+          <Mission.Block.Body.Container>
+            <Mission.Block.Body.Bold>
+              We are here to make your dreams come true!
+            </Mission.Block.Body.Bold>
+            <Mission.Block.Body.Text>
+              <br />
+              If you are dreaming about a new company, digital platform, mobile
+              app, or anything else? <br />
+              <br />
+              We can work on it together.
+            </Mission.Block.Body.Text>
+          </Mission.Block.Body.Container>
+        </Mission.Block.Column>
+      </Mission.Block.Container>
+    </Mission.Container>
   );
 }
 
-export default App;
+export default MissionView;
---
+++ src/views/Mission/Mission.style.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+const Mission = {};
+
+Mission.Container = styled.div`
+  margin: 330px 44px 150px;
+`;
+
+Mission.Title = styled.div`
+  max-width: 840px;
+  position: relative;
+  top: 36px;
+  left: 220px;
+`;
+
+Mission.Banner = styled.img``;
+
+Mission.Block = {
+  Container: styled.div`
+    display: flex;
+  `,
+  Column: styled.div``,
+  Title: {
+    Container: styled.div`
+      margin: 48px 0 0 230px;
+      color: #f56b00;
+      max-width: 300px;
+    `,
+    Bold: styled.div`
+      font-weight: 800;
+      font-size: 30px;
+      line-height: 36px;
+    `,
+    Emphasis: styled.div`
+      font-size: 30px;
+      line-height: 26px;
+      font-style: italic;
+      font-family: Libre Baskerville, Georgia;
+      margin-top: 6px;
+    `,
+  },
+  Body: {
+    Container: styled.div`
+      margin: 48px 0 0 70px;
+      color: #f56b00;
+      max-width: 300px;
+    `,
+    Bold: styled.div`
+      font-size: 16px;
+      font-weight: bold;
+      color: #646464;
+    `,
+    Text: styled.div`
+      font-size: 14px;
+      color: #646464;
+    `,
+  },
+};
+
+export default Mission;
